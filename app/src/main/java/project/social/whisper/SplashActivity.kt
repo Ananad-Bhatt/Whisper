f20dd7613aa6661341ff84b4d6d057bdf901b559
HET SHUKLA <SHUKLAHET2704@gmailc.com>
2024-01-10 00:52:20 +0530

    Create Splash screen

--- app/src/main/java/project/social/whisper/SplashActivity.kt
+++ app/src/main/java/project/social/whisper/SplashActivity.kt
@@ -1,11 +1,38 @@
 package project.social.whisper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import project.social.whisper.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        //Binding View
+        val b = ActivitySplashBinding.inflate(layoutInflater)
+        val view = b.root
+        setContentView(view)
+
+        //Creating animation variable
+        val slideLeftIn: Animation = AnimationUtils.loadAnimation(this, R.anim.slide_left_in)
+        val largeToSmall: Animation = AnimationUtils.loadAnimation(this, R.anim.large_to_small)
+
+        //Setting animation to views
+        b.splashLogo.startAnimation(slideLeftIn)
+        b.appName.startAnimation(largeToSmall)
+
+        //Creating delay for splash screen
+        Handler(Looper.getMainLooper()).postDelayed({
+            val login = Intent(this, LoginActivity::class.java)
+            startActivity(login)
+            finish()
+        }, 5000)
+
     }
 }
