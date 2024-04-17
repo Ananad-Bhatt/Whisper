b35409189e35cc055cabe5d6d5b58e7614a01787
HET SHUKLA <SHUKLAHET2704@gmailc.com>
2024-04-17 23:54:21 +0530

    Completed bug reporting

--- app/src/main/java/fragments/ReportBugFragment.kt
+++ app/src/main/java/fragments/ReportBugFragment.kt
@@ -1,11 +1,14 @@
 package fragments
 
+import adapters.DatabaseAdapter
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import project.social.whisper.R
+import project.social.whisper.databinding.FragmentReportBugBinding
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -16,7 +19,6 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ReportBugFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -31,9 +33,36 @@ class ReportBugFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report_bug, container, false)
+        val b = FragmentReportBugBinding.inflate(inflater, container, false)
+
+        b.btnSubmitBug.setOnClickListener {
+
+            if(b.edtBugReportFrag.text.trim().toString().isNotEmpty()
+                && b.edtBugReportMobileFrag.text.trim().toString().isNotEmpty()
+                && b.edtBugReportOsFrag.text.trim().toString().isNotEmpty())
+            {
+                try {
+                    val key = DatabaseAdapter.bugTable.push().key!!
+
+                    val ref = DatabaseAdapter.bugTable.child(key)
+
+                    ref.child("BUG").setValue(b.edtBugReportFrag.text.toString())
+                    ref.child("MOBILE").setValue(b.edtBugReportMobileFrag.text.toString())
+                    ref.child("VERSION").setValue(b.edtBugReportOsFrag.text.toString())
+                }catch (_:Exception){}
+            }
+            else{
+                if(isAdded)
+                {
+                    Toast.makeText(requireActivity(), "Fill all the information", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+        }
+
+        return b.root
     }
 
     companion object {
