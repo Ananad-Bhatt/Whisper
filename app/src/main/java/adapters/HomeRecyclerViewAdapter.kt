a95b624c0bf330e23715208ed793af00e5dcb96b
HET SHUKLA <SHUKLAHET2704@gmailc.com>
2024-04-22 17:45:24 +0530

    Store score in DB

--- app/src/main/java/adapters/HomeRecyclerViewAdapter.kt
+++ app/src/main/java/adapters/HomeRecyclerViewAdapter.kt
@@ -9,6 +9,9 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import models.HomeModel
 import project.social.whisper.R
 
@@ -31,6 +34,33 @@ class HomeRecyclerViewAdapter (private val postList:ArrayList<HomeModel>, privat
         var isUpClickable = true
         var isDownClickable = true
 
+        val dbPath = DatabaseAdapter.scoreTable.child(postList[position].key)
+            .child(postList[position].timeStamp).child(GlobalStaticAdapter.key)
+
+        dbPath.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists())
+                {
+                    val isUpVoted = snapshot.child("UPVOTED").getValue(Boolean::class.java)!!
+
+                    if(isUpVoted)
+                    {
+                        isUpClickable = false
+                        isDownClickable = true
+                    }
+                    else
+                    {
+                        isUpClickable = true
+                        isDownClickable = false
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+
+        })
+
         Glide.with(context).load(postList[position].img).into(holder.img)
         holder.title.text = postList[position].title
         holder.subTitle.text = postList[position].subTitle
@@ -44,15 +74,28 @@ class HomeRecyclerViewAdapter (private val postList:ArrayList<HomeModel>, privat
                 holder.score.text = (score).toString()
                 isUpClickable = false
                 isDownClickable = true
+
+                DatabaseAdapter.postTable.child(postList[position].key)
+                    .child(postList[position].timeStamp).child("SCORE")
+                    .setValue(score)
+
+                dbPath.child("UPVOTED").setValue(true)
             }
         }
 
         holder.downVote.setOnClickListener {
             //When user clicks down vote
             if(isDownClickable) {
-                holder.score.text = (holder.score.text.toString().toInt() - 1).toString()
+                val score = holder.score.text.toString().toInt() - 1
+                holder.score.text = (score).toString()
                 isDownClickable = false
                 isUpClickable = true
+
+                DatabaseAdapter.postTable.child(postList[position].key)
+                    .child(postList[position].timeStamp).child("SCORE")
+                    .setValue(score)
+
+                dbPath.child("UPVOTED").setValue(false)
             }
         }
     }
