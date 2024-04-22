4d0a37ff64e8b82a85ff0826d5f03957f3170cc6
HET SHUKLA <SHUKLAHET2704@gmailc.com>
2024-04-22 20:37:25 +0530

    Create Show Posts Activity

---
+++ app/src/main/java/project/social/whisper/ShowPostsActivity.kt
@@ -0,0 +1,11 @@
+package project.social.whisper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowPostsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_posts)
+    }
+}
